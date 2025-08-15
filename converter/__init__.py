e4de556af50bac135292c012caf98862741c803b
Luiz Eduardo <44537030+dadebr@users.noreply.github.com>
2025-08-15 12:52:10 -0300

    Create __init__.py

---
+++ converter/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Converter Package - Markitdown Converter
+
+Módulos responsáveis pela conversão de arquivos para Markdown.
+
+Módulos disponíveis:
+- file_converter: Conversão de arquivos individuais
+- batch_processor: Processamento em lote de múltiplos arquivos
+
+Author: dadebr
+GitHub: https://github.com/dadebr/markitdown-converter
+"""
+
+from .file_converter import FileConverter
+from .batch_processor import BatchProcessor
+
+__version__ = "1.0.0"
+__author__ = "dadebr"
+__email__ = "dadebr@github.com"
+__all__ = ["FileConverter", "BatchProcessor"]
